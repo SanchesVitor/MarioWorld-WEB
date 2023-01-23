64144fc37e41d6ac703b933cb400754bde495aaa
Vitor Sanches Ferreira <112482028+SanchesVitor@users.noreply.github.com>
2023-01-23 13:45:42 -0300

    add script

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+const botaoTrailer = document.querySelector('.botao-trailer');
+botaoTrailer.addEventListener('click', () => {
+  modal.classList
+});
+
+const modal = document.querySelector('.modal');
