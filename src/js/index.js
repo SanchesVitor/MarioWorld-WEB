6f8169b047721431efd4013d98d3fa8f1ec059fe
Vitor Sanches Ferreira <112482028+SanchesVitor@users.noreply.github.com>
2023-01-24 01:17:29 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,21 @@
 const botaoTrailer = document.querySelector('.botao-trailer');
+const modal = document.querySelector('.modal');
+const video = document.getElementById('video');
+const linkDoVideo = video.src;
+const botaoFecharModal = document.querySelector('.fechar-modal');
+
+function alternarModal() {
+  modal.classList.toggle('aberto');
+}
+
 botaoTrailer.addEventListener('click', () => {
-  modal.classList
+  alternarModal();
+  video.setAttribute('src', linkDoVideo);
 });
 
-const modal = document.querySelector('.modal');
+botaoFecharModal.addEventListener('click', () => {
+  alternarModal();
+  video.setAttribute('src', "");
+});
+
+
